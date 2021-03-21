571471fd2da3729d39d4c9b8ecb2af396a7348cb
ramitnagpalau <77922630+ramitnagpalau@users.noreply.github.com>
2021-03-21 21:16:41 +1100

    Update buildscript.sh
    Adding sudo

--- buildscript.sh
+++ buildscript.sh
@@ -1,13 +1,13 @@
 #Installing npm dependencies
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
-npm install
+sudo npm install
 
 #Installing yarn through npm
-npm install --global yarn
+sudo npm install --global yarn
 
 #Performing unit tests
-yarn test a --watchAll=false
+sudo yarn test a --watchAll=false
 
 #Creating Production build and compress archieve of production build
-yarn build && tar -zcf build.tar.gz build
+sudo yarn build && tar -zcf build.tar.gz build
