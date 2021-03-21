0b07263ec6ddd900dddfdbe6f93445cb87d0b29b
ramitnagpalau <77922630+ramitnagpalau@users.noreply.github.com>
2021-03-21 21:10:53 +1100

    Update localrun.sh
    Adding sudo

--- localrun.sh
+++ localrun.sh
@@ -1,13 +1,10 @@
 #Installing npm dependencies
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
-npm install
+sudo npm install
 
 #Installing yarn through npm
-npm install --global yarn
+sudo npm install --global yarn
 
 #Starting the development server
-yarn start
-~
-~
-
+sudo yarn start
