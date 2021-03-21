af7da498d66c1eeed47810d0d6aee6859b7d27aa
ramitnagpalau <77922630+ramitnagpalau@users.noreply.github.com>
2021-03-21 21:52:55 +1100

    Update buildscript.sh
    Adding serve

--- buildscript.sh
+++ buildscript.sh
@@ -1,3 +1,4 @@
+#NOTE: Considering below commands would run on remote host.
 #Installing npm dependencies
 curl -sL https://rpm.nodesource.com/setup_14.x | sudo -E bash -
 sudo yum install -y nodejs
@@ -11,3 +12,7 @@ sudo yarn test a --watchAll=false
 
 #Creating Production build and compress archieve of production build
 sudo yarn build && tar -zcf build.tar.gz build
+
+#Deployment
+sudo npm install -g serve
+sudo serve -s build
